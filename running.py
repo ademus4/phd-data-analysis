55ae77729faff632370ceb41bf2a5112df87debc
Adam Thornton <ademus4@gmail.com>
2021-04-20 18:57:25 +0100

    Wrapper tasks for running whole analysis

---
+++ running.py
@@ -0,0 +1,47 @@
+import luigi
+import os
+from glob import glob
+
+import pipeline
+
+
+class ProcessData(luigi.WrapperTask):
+    input_path = luigi.Parameter()
+    output_path = luigi.Parameter(
+        os.path.join(pipeline.DefaultParams().output_dir, "data"))
+
+    def requires(self):
+        # get a list of hipo files in the path and submit them as plotting tasks
+        input_files = glob(self.input_path)
+        for item in input_files:
+            yield pipeline.Plotting(item, output_dir=self.output_path)
+
+
+class ProcessSims(luigi.WrapperTask):
+    input_path = luigi.Parameter()
+    output_path = luigi.Parameter(
+        os.path.join(pipeline.DefaultParams().output_dir, "sims"))
+
+    def requires(self):
+        # get a list of hipo files in the path and submit them as plotting tasks
+        input_files = glob(self.input_path)
+        for item in input_files:
+            yield pipeline.Plotting(item, output_dir=self.output_path)
+
+
+class AnalyseMoments(luigi.WrapperTask):
+    data_path = luigi.Parameter()
+    sims_path = luigi.Parameter()
+
+    def requires(self):
+        # glob for correct files in data/sims dirs
+        # give these files to moments task
+        yield
+
+
+if __name__ == '__main__':
+    luigi.build(
+        [ProcessData()],
+        workers=1, 
+        local_scheduler=True
+    )
